24d366b746199747bd8cd8f5b68b5e8c763e7960
Andrey Polischuk <andre.polischuk@gmail.com>
2016-07-01 16:30:34 +0300

    Add eslint

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 import postcss from 'postcss';
+
 const styleRegExp = /(hidden|dotted|dashed|solid|double|groove|ridge|inset|outset)/i;
 
-export default postcss.plugin('postcss-border', () => {
-  return style => {
-    style.walkDecls('border', transform);
-  };
-});
-
 function transform(decl) {
-  const style = styleRegExp.exec(decl.value)[0];
-  let [size, color] = decl.value.split(`${style}`);
-  size = postcss.list.space(size);
-  color = postcss.list.space(color);
-  if (size.length < 2 && color.length < 2) return;
-  decl.value = `${size[0]} ${style} ${color[0]}`;
-  if (color.length > 1) decl.cloneAfter({prop: 'border-color', value: color.join(' ')});
-  if (size.length > 1) decl.cloneAfter({prop: 'border-width', value: size.join(' ')});
+  const [ style ] = styleRegExp.exec(decl.value);
+  const [ size, color ] = decl.value.split(`${style}`);
+  const sizeList = postcss.list.space(size);
+  const colorList = postcss.list.space(color);
+
+  if (sizeList.length < 2 && colorList.length < 2) return;
+
+  /* eslint-disable no-param-reassign */
+  decl.value = `${sizeList[0]} ${style} ${colorList[0]}`;
+
+  if (colorList.length > 1) {
+    decl.cloneAfter({ prop: 'border-color', value: colorList.join(' ') });
+  }
+
+  if (sizeList.length > 1) {
+    decl.cloneAfter({ prop: 'border-width', value: sizeList.join(' ') });
+  }
 }
+
+export default postcss.plugin('postcss-border', () => style => {
+  style.walkDecls('border', transform);
+});
