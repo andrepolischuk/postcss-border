ac1615b583328fe22bfa58805a3253796abfacad
Andrey Polischuk <andre.polischuk@gmail.com>
2016-07-01 16:12:15 +0300

    Add ava instead of mocha

--- test.js
+++ test.js
@@ -1,32 +1,29 @@
-import border from './index';
+import test from 'ava';
 import postcss from 'postcss';
-import assert from 'assert';
+import border from './index';
 
-it('should return original', done => {
-  verify('a { border: 1px solid #000; }', 'a { border: 1px solid #000; }', done);
+const transform = input => postcss([ border ]).process(input);
+
+test('return original', async t => {
+  const res = await transform('a { border: 1px solid #000; }');
+  t.is(res.warnings().length, 0);
+  t.is(res.css, 'a { border: 1px solid #000; }');
 });
 
-it('should set border and border-width', done => {
-  verify('a { border: 1px 0 solid #000; }', 'a { border: 1px solid #000; border-width: 1px 0; }', done);
+test('set border and border-width', async t => {
+  const res = await transform('a { border: 1px 0 solid #000; }');
+  t.is(res.warnings().length, 0);
+  t.is(res.css, 'a { border: 1px solid #000; border-width: 1px 0; }');
 });
 
-it('should set border and border-color', done => {
-  verify('a { border: 1px solid #444 #222 #000; }', 'a { border: 1px solid #444; border-color: #444 #222 #000; }', done);
+test('set border and border-color', async t => {
+  const res = await transform('a { border: 1px solid #444 #222 #000; }');
+  t.is(res.warnings().length, 0);
+  t.is(res.css, 'a { border: 1px solid #444; border-color: #444 #222 #000; }');
 });
 
-it('should set border, border-style and border-color', done => {
-  verify('a { border: 0 1px 2px solid #000 #333; }', 'a { border: 0 solid #000; border-width: 0 1px 2px; border-color: #000 #333; }', done);
+test('set border, border-style and border-color', async t => {
+  const res = await transform('a { border: 0 1px 2px solid #000 #333; }');
+  t.is(res.warnings().length, 0);
+  t.is(res.css, 'a { border: 0 solid #000; border-width: 0 1px 2px; border-color: #000 #333; }');
 });
-
-function verify(input, out, done) {
-  postcss([border()])
-    .process(input)
-    .then(res => {
-      assert(res.warnings().length === 0);
-      assert(res.css === out);
-      done();
-    })
-    .catch(err => {
-      done(err);
-    });
-}
